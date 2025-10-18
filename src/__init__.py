c1e1f1a27ddafd0317780ea306743898a41bba8b
Philippe Nunes <philippenunes@live.com>
2025-10-17 23:42:51 -0300

    config

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 from src.books import router as book_router
 from fastapi.middleware.cors import CORSMiddleware  
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"server is starting...")
+    await init_db()
+    yield
+    print(f"server has been stopped")
 
 version = "v1"
 
 app = FastAPI(
     title="Bookly",
     description="A REST API for a book review web service",
-    version = version
+    version = version,
+    lifespan=life_span
 )
 
 # Permite requisições de qualquer origem (apenas para desenvolvimento)
