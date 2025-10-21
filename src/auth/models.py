5758a4a01902c1a351b265653f8ef42133f67730
Philippe Nunes <philippenunes@live.com>
2025-10-21 00:01:20 -0300

    alembic config and include User model

---
+++ src/auth/models.py
@@ -0,0 +1,27 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime
+from uuid import UUID, uuid4
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    uid: UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            nullable=False,
+            default=uuid4,
+        )
+    )
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool = Field(default=False)
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+    def __repr__(self) -> str:
+        return f"<User {self.username}>"
