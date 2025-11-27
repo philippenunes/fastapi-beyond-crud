4d1edd08d6db922d2fb836c81e2e5298a1fc86fc
Philippe Nunes <philippenunes@live.com>
2025-11-26 23:50:05 -0300

    creating a user book model

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -23,6 +23,9 @@ class UserModel(BaseModel):
     password_hash: str = Field(exclude=True)
     created_at: datetime
     updated_at: datetime
+
+
+class UserBooksModel(UserModel):
     books: List[Book]
 
 
